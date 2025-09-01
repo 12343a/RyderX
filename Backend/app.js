d5758c471e980a79c1b0f216bb880dc70c2e3e63
12343a <2k22.csai.2213430@gmail.com>
2025-09-01 12:15:18 +0530

    my new changes

---
+++ Backend/app.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
+
+connectToDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",  // React frontend ka port (Vite ke liye mostly 5173 hota hai)
+  credentials: true                 // cookies/tokens send karne ke liye
+}));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
+
+module.exports = app;
+
